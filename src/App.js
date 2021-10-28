c208710f1b74e8e0d8c7e7d0fea9540ed9c3b05e
theo gandara <theogandara@gmail.com>
2021-10-27 23:45:35 -0300

    feature: realizado o ex de children

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import CenteredCard from "./components/CenteredCard";
+import "./components/Centered-style.css"
+
+
+function App() {
+
+  return (
+    <CenteredCard>
+      <h1>Ronaldinho Gaúcho</h1>
+      <p>Foi um jogador que conquistou as torcidas de todos os clubes por onde jogou, com seus dribles diferentes e chutes impressionantes.</p>
+      <p>Já passou por diversos clubes incluindo Barcelona, PSG e Flamengo.</p>
+    </CenteredCard>
+  );
+}
+
+export default App;
---
+++ src/components/CenteredCard.js
@@ -0,0 +1,9 @@
+const CenteredCard = ({children}) => (
+    <div className = "card">
+        <span>
+            {children}
+        </span>
+    </div>
+)
+
+export default CenteredCard
